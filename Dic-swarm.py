4cc717b6542f4249d15e9b552dfb8a877a373dea
602344037@qq.com <602344037@qq.com>
2017-12-29 20:50:09 +0800

    加入了寻找词组功能

--- Dic-swarm.py
+++ Dic-swarm.py
@@ -11,27 +11,32 @@ def getResponse(url):
     except:
         print("请求失败")
 
-def searchtag(html,tag):
-    soup=BeautifulSoup(html,"html.parser")
-    tlist=[]
-    for item in soup.find(tag):
-        if isinstance(tag,bs4.element.tag):
-            tlist.append([tag.string])
+def searchtag(html):
+    ilist=re.findall(r"<a class=\"search-js\" href=\".*?#keyfrom=dict.basic.wordgroup\">.*?</span>",html)  
+    return ilist
 
 def searchre(html):
-        refind=re.findall(r"<span class=\"text\">.*</span>",html)
-        return refind
+    refind=re.findall(r"<span class=\"text\">.*</span>",html)
+    return refind
 
-def formurl(word):
+def formurlforsynonym(word):
     url="http://www.thesaurus.com/browse/"+word+"?s=t"
+    return url
+
+def formurlforphrase(word):
+    url="http://dict.youdao.com/w/"+word+"/#keyfrom=dict2.top"
     return url
 
 def search():
     word=input("please enter a word：")
-    url=formurl(word)
-    html=getResponse(url)
-    words=searchre(html)
+    url1=formurlforsynonym(word)
+    url2=formurlforphrase(word)
+    html1=getResponse(url1)
+    html2=getResponse(url2)
+    words=searchre(html1)
+    phrases=searchtag(html2)
     i=0
+    print("Here are some synonyms !\n")
     for w in words:
         i=i+1
         h1=w.split(">")
@@ -39,5 +44,17 @@ def search():
         print(h2[0])
         if(i>=7):
             break
+    j=0
+    print("\nHere are phrases !\n")
+    for n in phrases:
+        j=j+1
+        h1=n.split("wordgroup\">")
+        h2=h1[1].split("</a>")
+        print(h2[0])
+        if(j>=10):
+            break
+    print("\n")
+
+    
 while(1):
     search()
