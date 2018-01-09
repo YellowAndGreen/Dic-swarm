9eca420b425dca43feb826ec9d2260eaec8d0939
602344037@qq.com <602344037@qq.com>
2018-01-09 15:08:45 +0800

    匹配了[]

--- Dic-swarm.py
+++ Dic-swarm.py
@@ -20,22 +20,22 @@ def searchre(html):
     return refind
 def searchsynonym(html):
     words=[]
-    refindn=re.search(r"n\.\s*[\u4e00-\u9fa5]*?；",html)
+    refindn=re.search(r"n\.[\s|\u4e00-\u9fa5|，|；|\[|\]]*?<\/li>",html)
     if refindn:
         words.append(refindn.group(0))
     else:
         words.append("none")
-    refindvt=re.search(r"vt\.\s*[\u4e00-\u9fa5|，]*?；",html)
+    refindvt=re.search(r"vt\.[\s|\u4e00-\u9fa5|，|；|\[|\]]*?<\/li>",html)
     if refindvt:
         words.append(refindvt.group(0))
     else:
         words.append("none")
-    refindvi=re.search(r"vi\.\s*[\u4e00-\u9fa5|，]*?；",html)
+    refindvi=re.search(r"vi\.[\s|\u4e00-\u9fa5|，|；|\[|\]]*?<\/li>",html)
     if refindvi:
         words.append(refindvi.group(0))
     else:
         words.append("none")
-    refindadj=re.search(r"adj\.\s*[\u4e00-\u9fa5|，]*?；",html)
+    refindadj=re.search(r"adj\.[\s|\u4e00-\u9fa5|，|；|\[|\]]*?<\/li>",html)
     if refindadj:
         words.append(refindadj.group(0))
     else:
@@ -66,8 +66,12 @@ def search():
         h1=w.split(">")
         h2=h1[1].split("<")
         htmlforsyn=getResponse(formurlforphrase(h2[0]))
-        wordss=searchsynonym(htmlforsyn)
-        print(tplt.format(h2[0],wordss[0],wordss[1],wordss[2],wordss[3]))
+        wordss1=searchsynonym(htmlforsyn)
+        wordss2=[]
+        for wordsss in wordss1:
+            wordsplit=wordsss.split("<")
+            wordss2.append(wordsplit[0])
+        print(tplt.format(h2[0],wordss2[0],wordss2[1],wordss2[2],wordss2[3]))
         if(i>=12):
             break
     j=0
